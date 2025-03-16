71451e26e09af901c52e5b1438061cad9408acb2
Mohammad Amer <mohammad.amer@atea.com>
2025-03-16 23:50:38 +0200

    Minor changes to PnPJsHandler and yarn

--- src/extensions/copilotInSearch/Interfaces/IPnPJsHandler.ts
+++ src/extensions/copilotInSearch/Interfaces/IPnPJsHandler.ts
@@ -1,7 +1,7 @@
 import { ApplicationCustomizerContext } from "@microsoft/sp-application-base";
 import { ISiteUserInfo } from "@pnp/sp/site-users/types";
 
-export default interface ISharePointHandler {
+export default interface IPnPJsHandler {
     init(context?: ApplicationCustomizerContext): void 
     getCopilotConfigrations(): Promise<any>;
     getCurrentUser(): Promise<ISiteUserInfo>;
